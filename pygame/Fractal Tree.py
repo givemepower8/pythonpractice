95065b3319af24afe497994c2151564501529097
Wei Wang <givemepower8@gmail.com>
2018-09-01 11:25:48 -0700

    refactor

--- pygame/Fractal Tree.py
+++ pygame/Fractal Tree.py
@@ -7,21 +7,21 @@ pygame.display.set_caption("Fractal Tree")
 screen = pygame.display.get_surface()
 
 
-def drawTree(x1, y1, angle, depth):
+def draw_tree(x1: float, y1: float, angle: float, depth: int):
     if depth:
         x2 = x1 + int(math.cos(math.radians(angle)) * depth * 10.0)
         y2 = y1 + int(math.sin(math.radians(angle)) * depth * 10.0)
         pygame.draw.line(screen, (255, 255, 255), (x1, y1), (x2, y2), 2)
-        drawTree(x2, y2, angle - 20, depth - 1)
-        drawTree(x2, y2, angle + 20, depth - 1)
+        draw_tree(x2, y2, angle - 20, depth - 1)
+        draw_tree(x2, y2, angle + 20, depth - 1)
 
 
-def input(event):
+def wait_input(event):
     if event.type == pygame.QUIT:
         exit(0)
 
 
-drawTree(300, 550, -90, 9)
+draw_tree(300, 550, -90, 9)
 pygame.display.flip()
 while True:
-    input(pygame.event.wait())
+    wait_input(pygame.event.wait())
